d90d5c9264c55b917cd305ad220452354c4ba956
Tyler Bisig <bisig1892@gmail.com>
2025-05-21 21:13:16 -0700

    Created Angular Services able to submit form and save to db after CORS was enabled on API

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-add-category',
@@ -10,7 +11,7 @@ export class AddCategoryComponent {
 
   model: AddCategoryRequest;
 
-  constructor() {
+  constructor(private categoryService: CategoryService) {
     this.model = {
       name: '',
       urlHandle: ''
@@ -18,6 +19,16 @@ export class AddCategoryComponent {
   }
 
   onSubmit() {
+    this.categoryService.addCategory(this.model).subscribe({
+      next: (response) => {
+        console.log('Category added successfully');
+        // Optionally, reset the form or navigate to another page
+      },
+      error: (error) => {
+        console.error('Error adding category', error);
+        // Handle error appropriately
+      }
+    });
   }
 
 }
