50055edc2e894cea3991ea816daa6437203998f4
Tyler Bisig <bisig1892@gmail.com>
2025-06-12 21:59:16 -0700

    Securely storing JWT Token on UI ngx-cookie-service v16 installed

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { LoginRequest } from '../models/login-request.mode';
 import { AuthService } from '../services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +13,7 @@ export class LoginComponent {
 
   model: LoginRequest;
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private cookieService: CookieService, private router: Router) {
     this.model = {
       email: '',
       password: ''
@@ -21,7 +23,12 @@ export class LoginComponent {
   onFormSubmit(): void {
     this.authService.login(this.model).subscribe({
       next: (response) => {
-        console.log(response);
+        // set Auth Cookie
+        this.cookieService.set('Authorization', `Bearer ${response.token}`,
+          undefined, '/', undefined, true, 'Strict');
+
+        // redirect back to homepage
+        this.router.navigateByUrl('/');
       }
     });
 
